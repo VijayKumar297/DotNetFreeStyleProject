8b8b3f0e4bdcb3b75ddaef93a1cc8a09fa38df2d
Vijay <vijaykumar2907@outlook.com>
2023-09-22 21:16:08 +0530

    Add project files.

---
+++ DotNetFreeStyleProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetFreeStyleProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
